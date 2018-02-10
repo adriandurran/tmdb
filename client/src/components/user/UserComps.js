b772442be64c4aef853ff535f1e34d367de91140
Adrian <adrian@intellidroid.eu>
2018-02-10 21:46:34 +0000

    added some comments on components

--- client/src/components/user/UserComps.js
+++ client/src/components/user/UserComps.js
@@ -4,6 +4,14 @@ import _ from 'lodash';
 
 class UserComps extends Component {}
 
-function mapStateToProps({ comps }, ownProps) {}
+function mapStateToProps({ comps, courses }, ownProps) {
+  // iterate of the courses state to get the full details
+  // compare pass date and validity
+  // merge (or ignore courses past validity)
+  // iterate over the comps state and check combination of user courses = competency
+  // some things to think about....if the user has the courses for a  competency....but one of his courses
+  // has expired...do we show the competency? Do we highlight this to the user in some way?
+  // I think we need to tie the courses and competencies together in a clearly understandable way
+}
 
 export default connect(mapStateToProps)(UserComps);
--- client/src/components/user/UserCourses.js
+++ client/src/components/user/UserCourses.js
@@ -27,6 +27,10 @@ class UserCourses extends Component {
 function mapStateToProps({ courses }, ownProps) {
   // need to create a filter......then union
   //   work in progress
+  //   do we show courses that have expired?
+
+  // To do add some styling to warn if course is about to expire?
+  // allow courses to be clickable (launch a modal component) to show details
   //   ****** do not change this *****
 
   return {
