fb60abfde13a07f6d11ff959a58a06b50ea6adce
Adrian <adrian@intellidroid.eu>
2018-02-13 20:03:02 +0000

    merged filtered course list with pass dates

--- client/src/components/user/UserCourses.js
+++ client/src/components/user/UserCourses.js
@@ -7,7 +7,8 @@ class UserCourses extends Component {
     return courses.map(course => {
       return (
         <li className="collection-item" key={course.courseId}>
-          {course.coursename}, Valid for: {course.validity} months
+          {course.coursename}, Valid for: {course.validity} months, Pass date:{' '}
+          {course.passDate}
         </li>
       );
     });
@@ -29,23 +30,5 @@ const mapStateToProps = state => {
     userCourses: selectUserCourseNames(state)
   };
 };
-
-// function mapStateToProps({ courses }, ownProps) {
-//   // need to create a filter......then union
-//   //   work in progress
-//   //   do we show courses that have expired?
-
-//   // To do add some styling to warn if course is about to expire?
-//   // allow courses to be clickable (launch a modal component) to show details
-//   //   ****** do not change this *****
-
-//   return {
-//     userCoursesFull: _.filter(courses, x =>
-//       _.includes(_.map(ownProps.ucourses, 'courseId'), x.courseId)
-//     )
-
-//     // _.unionBy(userCourses, ownProps.ucourses, 'courseId')
-//   };
-// }
 
 export default connect(mapStateToProps)(UserCourses);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -33,10 +33,14 @@ export const selectCourses = state => state.courses;
 export const selectUserCourseNames = createSelector(
   selectUserCourses,
   selectCourses,
-  (userCourses, coursesList) =>
-    _.filter(coursesList, x =>
+  (userCourses, coursesList) => {
+    const filteredList = _.filter(coursesList, x =>
       _.includes(_.map(userCourses, 'courseId'), x.courseId)
-    )
+    );
+    return _.map(filteredList, obj => {
+      return _.assign(obj, _.find(userCourses, { courseId: obj.courseId }));
+    });
+  }
 );
 
 export default reducer;
