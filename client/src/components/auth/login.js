ac84f93dc6f55b07577c5ea7e0cffc9bb0b1ef9b
Adrian <adrian@intellidroid.eu>
2018-02-18 10:18:25 +0000

    added notes

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+// use redux form
+// will mock the actuall login process until set up with a db
+
 class LoginUser extends Component {
   render() {
     return <div>Login component</div>;
