91ad98ed730030bf259a6cb3164f0ca1b9f46038
Adrian <adrian@intellidroid.eu>
2018-02-18 10:52:46 +0000

    mocked out login form

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 // use redux form
 // will mock the actuall login process until set up with a db
 
 class LoginUser extends Component {
+  renderFields() {
+    return (
+      <div className="row">
+        <div className="col s6">
+          <Field
+            type="email"
+            name="email"
+            component="input"
+            placeholder="email address"
+          />
+        </div>
+        <div className="col s6">
+          <Field
+            type="password"
+            name="pwd"
+            component="input"
+            placeholder="password"
+          />
+        </div>
+      </div>
+    );
+  }
+
   render() {
-    return <div>Login component</div>;
+    const { handleSubmit, submitting, pristine } = this.props;
+    return (
+      <div className="row">
+        <div className="col s12 m10offset-m1">
+          <div className="card-panel">
+            <form onSubmit={handleSubmit(values => console.log(values))}>
+              {this.renderFields()}
+              <div className="row">
+                <button
+                  type="submit"
+                  className="waves-effect waves-light btn right"
+                  disabled={pristine || submitting}
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default LoginUser;
+export default reduxForm({
+  form: 'loginForm'
+})(LoginUser);
