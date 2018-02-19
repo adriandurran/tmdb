e3cbb20055094a27d24198b5ca2c51c2a3c9230d
Adrian <adrian@intellidroid.eu>
2018-02-19 17:09:18 +0000

    added an email key to users

--- client/src/components/auth/registerField.js
+++ client/src/components/auth/registerField.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-export default ({ input, meta: { error, touched } }) => {
+export default ({ input, label, type, meta: { error, touched, warning } }) => {
   return (
     <div>
-      <input {...input} style={{ marginBottom: '5px' }} />
+      <input
+        {...input}
+        placeholder={label}
+        type={type}
+        style={{ marginBottom: '5px' }}
+      />
       <div className="red-text" style={{ marginBottom: '20px' }}>
-        {touched && error}
+        {touched &&
+          ((error && <span>{error}</span>) ||
+            (warning && <span>{warning}</span>))}
       </div>
     </div>
   );
