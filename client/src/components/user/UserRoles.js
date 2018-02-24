59f86c11ad4a67c81beea4b0310b6d07d6c3d257
Adrian <adrian@intellidroid.eu>
2018-02-24 16:36:23 +0000

    added key to userroles

--- client/src/components/user/UserRoles.js
+++ client/src/components/user/UserRoles.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { selectUserRoleNames } from '../../reducers/selectors';
 
+import { withStyles } from 'material-ui/styles';
+import withRoot from '../../withRoot';
+import rootStyles from '../../styles/rootStyle';
+
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 
 class UserRoles extends Component {
   renderRoles(roles) {
     return roles.map(role => {
       return (
-        <ListItem>
+        <ListItem key={role.roleId}>
           <ListItemText primary={role.rolename} />
         </ListItem>
       );
@@ -19,9 +23,7 @@ class UserRoles extends Component {
     return (
       <div>
         <h3>Roles</h3>
-        <List>
-          {this.renderRoles(this.props.userRoles)}
-        </List>
+        <List>{this.renderRoles(this.props.userRoles)}</List>
       </div>
     );
   }
@@ -33,4 +35,6 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(UserRoles);
+UserRoles = withStyles(rootStyles)(UserRoles);
+
+export default withRoot(connect(mapStateToProps)(UserRoles));
