f493c5807707fc0e2d2dd296212ddc700fc29627
Adrian <adrian@intellidroid.eu>
2018-03-25 10:02:17 +0100

    feat: added admin dashboard

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Card, { CardContent } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
+
+import { withStyles } from 'material-ui/styles';
+import withRoot from '../../withRoot';
+import rootStyles from '../../styles/rootStyle';
 
 const AdminDashboard = props => {
-  return <div>Admin Dashboard</div>;
+  const { classes } = props;
+  return (
+    <div>
+      <Card raised>
+        <CardContent className={classes.card}>
+          <Typography variant="display3" gutterBottom align="center">
+            Admin Dashboard
+          </Typography>
+          <hr />
+          <Grid container spacing={8}>
+            <Grid item xs={6}>
+              <Button
+                size="large"
+                variant="raised"
+                className={classes.dashboardButton}
+                component={Link}
+                to={'/admin/course-builder'}
+              >
+                Course Builder
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
-export default AdminDashboard;
+export default withRoot(withStyles(rootStyles)(AdminDashboard));
--- client/src/styles/rootStyle.js
+++ client/src/styles/rootStyle.js
@@ -1,43 +1,44 @@
 const rootStyles = theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
 
   flex: {
-    flex: 1
+    flex: 1,
   },
+  dashboardButton: {},
   paper: {
     marginTop: theme.spacing.unit * 5,
     marginBottom: theme.spacing.unit * 2,
-    padding: theme.spacing.unit * 2
+    padding: theme.spacing.unit * 2,
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20
+    marginRight: 20,
   },
   card: {
     marginTop: theme.spacing.unit * 10,
-    marginBottom: theme.spacing.unit * 10
+    marginBottom: theme.spacing.unit * 10,
     // color: theme.palette.primary.contrastText,
     // backgroundColor: theme.palette.primary.main
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200
+    width: 200,
   },
   appbarTitle: {
     textDecoration: 'none',
-    flex: 1
+    flex: 1,
   },
   formContainer: {
     display: 'flex',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
   },
 
   adderPaper: {
     marginTop: theme.spacing.unit * 1,
-    padding: theme.spacing.unit * 2
+    padding: theme.spacing.unit * 2,
     // display: 'flex'
   },
   formFields: {
@@ -45,8 +46,8 @@ const rootStyles = theme => ({
     marginLeft: theme.spacing.unit * 2,
     marginRight: theme.spacing.unit * 2,
     marginBottom: theme.spacing.unit * 2,
-    width: 400
-  }
+    width: 400,
+  },
 });
 
 export default rootStyles;
