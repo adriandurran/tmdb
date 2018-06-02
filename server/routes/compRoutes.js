e3cb81f0935fb1e1c2b6643eca8e1b3f4d630a52
Adrian Durran <adrian@intellidroid.eu>
2018-06-02 16:57:23 +0100

    feat: added comptype route and controller

--- server/routes/compRoutes.js
+++ server/routes/compRoutes.js
@@ -4,6 +4,13 @@ const compController = require('../controllers/compController');
 
 module.exports = app => {
   app.get('/api/competencies', requireLogin, compController.getComps);
-
   app.post('/api/competencies', requireAdmin, compController.addComps);
+
+  app.get('/api/competencies/type', requireLogin, compController.getCompTypes);
+  app.post('/api/competencies/type', requireAdmin, compController.addCompType);
+  app.delete(
+    '/api/comptetencies/type',
+    requireAdmin,
+    compController.deleteCompType
+  );
 };
