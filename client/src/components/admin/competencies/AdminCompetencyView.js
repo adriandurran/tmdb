ca11896e2f910671277b9fbf48f355d139845d32
Adrian Durran <adrian@intellidroid.eu>
2018-06-26 08:46:34 +0100

    feat: adding edit form to comp view

--- client/src/components/admin/competencies/AdminCompetencyView.js
+++ client/src/components/admin/competencies/AdminCompetencyView.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
 
-import { Header, Grid } from 'semantic-ui-react';
+import { Header, Grid, Breadcrumb } from 'semantic-ui-react';
+
+import { selectCompetency } from '../../../reducers/selectors/compSelectors';
 
 import AdminEditCompetency from './AdminEditCompetency';
 import AdminCompetencyHolders from './AdminCompetencyHolders';
 
-const AdminCompetencyView = () => {
-  return (
-    <div>
-      <Header as="h2" textAlign="center">
-        Competency View
-      </Header>
-      <Grid columns={2} centered>
-        <Grid.Column>
-          <AdminEditCompetency />
-        </Grid.Column>
-        <Grid.Column>
-          <AdminCompetencyHolders />
-        </Grid.Column>
-      </Grid>
-    </div>
-  );
+class AdminCompetencyView extends Component {
+  render() {
+    const { comp } = this.props;
+    return (
+      <div>
+        <Header as="h2" textAlign="center">
+          Competency View
+        </Header>
+        <Breadcrumb style={{ marginBottom: '2em' }}>
+          <Breadcrumb.Section link as={Link} to="/admin/dashboard">
+            Admin Dashboard
+          </Breadcrumb.Section>
+          <Breadcrumb.Divider icon="right chevron" />
+          <Breadcrumb.Section link as={Link} to="/admin/comp-manager">
+            Competency Manager
+          </Breadcrumb.Section>
+          <Breadcrumb.Divider icon="right arrow" />
+          <Breadcrumb.Section active>
+            {_.isEmpty(comp)
+              ? 'No Competency found'
+              : `Details for ${comp.compName}`}
+          </Breadcrumb.Section>
+        </Breadcrumb>
+        <Grid columns={2} centered>
+          <Grid.Column>
+            <AdminEditCompetency />
+          </Grid.Column>
+          <Grid.Column>
+            <AdminCompetencyHolders />
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    comp: selectCompetency(state)
+  };
 };
+
+AdminCompetencyView = connect(mapStateToProps)(AdminCompetencyView);
 
 export default AdminCompetencyView;
--- client/src/components/admin/competencies/AdminEditCompetency.js
+++ client/src/components/admin/competencies/AdminEditCompetency.js
@@ -1,16 +1,117 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { reduxForm, Field } from 'redux-form';
+import _ from 'lodash';
+
+import { Header, Form, Dropdown, Button } from 'semantic-ui-react';
+
+import semanticFormField from '../../shared/semanticFormField';
 
 import { clearCompetency } from '../../../actions/comps';
-import { selectCompetency } from '../../../reducers/selectors/compSelectors';
+import { selectCoursesForDropDown } from '../../../reducers/selectors/courseSelectors';
+import {
+  selectCompetency,
+  selectCompetencyTypesForDropDown
+} from '../../../reducers/selectors/compSelectors';
 
 class AdminEditCompetency extends Component {
   componentWillUnmount() {
     this.props.clearCompetency();
   }
 
+  selectedCompType() {
+    const { comp } = this.props;
+    if (_.isEmpty(comp) || !comp.compType) {
+      return '';
+    }
+
+    return comp.compType._id;
+  }
+
+  selectedCourses() {
+    const { comp } = this.props;
+    if (_.isEmpty(comp)) {
+      return [];
+    }
+
+    return comp.courses.map(course => course._id);
+  }
+
   render() {
-    return <div>Edit Competency</div>;
+    const {
+      courses,
+      compTypes,
+      handleSubmit,
+      submitting,
+      pristine
+    } = this.props;
+    return (
+      <div>
+        <Header as="h3" textAlign="center">
+          Edit Competency
+        </Header>
+        <Form>
+          <Form.Group inline widths="equal">
+            <Field
+              fluid
+              component={semanticFormField}
+              as={Form.Input}
+              type="text"
+              name="shortName"
+              placeholder="Short name"
+            />
+            <Field
+              fluid
+              name="compName"
+              component={semanticFormField}
+              as={Form.Input}
+              type="text"
+              placeholder="Competency Name"
+            />
+          </Form.Group>
+          <Form.Group>
+            <Dropdown
+              selection
+              name="compTypes"
+              options={compTypes}
+              placeholder="Select a Competency Type"
+              onChange={this.handleSelectCompChange}
+              value={this.selectedCompType()}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Dropdown
+              fluid
+              selection
+              multiple
+              name="compCourses"
+              options={courses}
+              placeholder="Select a Course"
+              onChange={this.handleSelectChange}
+              value={this.selectedCourses()}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Button
+              fluid
+              disabled={pristine || submitting}
+              type="submit"
+              size="medium"
+            >
+              Update Competency
+            </Button>
+            <Button
+              fluid
+              disabled={pristine || submitting}
+              type="submit"
+              size="medium"
+            >
+              Reset
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
+    );
   }
 }
 
@@ -20,7 +121,9 @@ const mapDispatchToProps = {
 
 const mapStateToProps = state => {
   return {
-    comp: selectCompetency(state)
+    comp: selectCompetency(state),
+    courses: selectCoursesForDropDown(state),
+    compTypes: selectCompetencyTypesForDropDown(state)
   };
 };
 
@@ -29,4 +132,6 @@ AdminEditCompetency = connect(
   mapDispatchToProps
 )(AdminEditCompetency);
 
-export default AdminEditCompetency;
+export default reduxForm({
+  form: 'editComp'
+})(AdminEditCompetency);
