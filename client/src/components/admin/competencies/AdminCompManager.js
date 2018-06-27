e4b502dc6c4b7159c566a680691cc1cb9b127512
Adrian Durran <adrian@intellidroid.eu>
2018-06-27 10:24:19 +0100

    feat: added shared menu to role and comp manager

--- client/src/components/admin/competencies/AdminCompManager.js
+++ client/src/components/admin/competencies/AdminCompManager.js
@@ -5,24 +5,28 @@ import { Grid } from 'semantic-ui-react';
 import CompBuilder from './CompBuilder';
 import AdminCompsTable from './AdminCompsTable';
 import AdminCompTypes from './AdminCompTypes';
+import AdminManagerMenu from '../shared/AdminManagerMenu';
 
 const AdminCompManager = props => {
   return (
-    <Grid celled>
-      <Grid.Row columns={2}>
-        <Grid.Column width={10}>
-          <CompBuilder />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <AdminCompTypes />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column>
-          <AdminCompsTable />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <div>
+      <AdminManagerMenu />
+      <Grid celled attached="bottom">
+        <Grid.Row columns={2}>
+          <Grid.Column width={10}>
+            <CompBuilder />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <AdminCompTypes />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <AdminCompsTable />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 };
 
--- client/src/components/admin/roles/AdminRoleManager.js
+++ client/src/components/admin/roles/AdminRoleManager.js
@@ -4,21 +4,25 @@ import { Grid } from 'semantic-ui-react';
 
 import RoleBuilder from './RoleBuilder';
 import AdminRolesTable from './AdminRolesTable';
+import AdminManagerMenu from '../shared/AdminManagerMenu';
 
 const AdminRoleManager = props => {
   return (
-    <Grid celled>
-      <Grid.Row>
-        <Grid.Column>
-          <RoleBuilder />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column>
-          <AdminRolesTable />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <div>
+      <AdminManagerMenu />
+      <Grid celled attached="bottom">
+        <Grid.Row>
+          <Grid.Column>
+            <RoleBuilder />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <AdminRolesTable />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 };
 
