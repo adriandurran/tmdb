9fd33fc3d8b3edaa8324bb302fc21bce7dab557b
Adrian Durran <adrian@intellidroid.eu>
2018-06-28 07:56:07 +0100

    feat: added in dept routes (server)

---
+++ server/routes/deptRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const requireLogin = require('../middlewares/requireLogin');
+const requireAdmin = require('../middlewares/requireAdmin');
+
+const deptController = require('../controllers/deptController');
+
+router.get('/', requireLogin, deptController.getDepts);
+router.post('/', requireAdmin, deptController.addDept);
+router.put('/:id', requireAdmin, deptController.updateDept);
+
+module.exports = router;
