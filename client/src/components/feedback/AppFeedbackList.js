934f98e79ef65d586e1f42b7587db0f4fb9ca1b4
Adrian Durran <adrian@intellidroid.eu>
2018-07-14 19:04:18 +0100

    feat: adding in feedback list

--- client/src/components/feedback/AppFeedbackList.js
+++ client/src/components/feedback/AppFeedbackList.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+import { selectFeedBackDateDesc } from '../../reducers/selectors/extraSelectors';
+
 class AppFeedbackList extends Component {
   render() {
     return <div>List feedback</div>;
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    feedback: selectFeedBackDateDesc(state)
+  };
+};
 
 export default AppFeedbackList;
