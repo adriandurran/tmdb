396459ae6dd30c55f25c31110024ad50994e1254
Adrian Durran <adrian@intellidroid.eu>
2018-07-14 11:52:53 +0100

    feat: scaffolding out feedback area

--- client/src/components/feedback/AppFeedback.js
+++ client/src/components/feedback/AppFeedback.js
@@ -3,7 +3,21 @@ import React from 'react';
 import { Header, Grid } from 'semantic-ui-react';
 
 const AppFeedback = () => {
-  return <div>App feedback</div>;
+  return (
+    <div>
+      <Header as="h2" textAlign="center">
+        Application Feedback &amp; Error reporting
+      </Header>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column>Add feedback</Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>list feedback</Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 };
 
 export default AppFeedback;
