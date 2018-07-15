c5aa836983679559dcab67594911f9ce2bbe2344
Adrian Durran <adrian@intellidroid.eu>
2018-07-15 09:16:05 +0100

    feat: added in feedback list

--- client/src/components/feedback/AppFeedbackList.js
+++ client/src/components/feedback/AppFeedbackList.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+
+import { Item, Header } from 'semantic-ui-react';
 
 import { selectFeedBackDateDesc } from '../../reducers/selectors/extraSelectors';
+import { fetchFeedback } from '../../actions/extra';
 
 class AppFeedbackList extends Component {
+  componentDidMount() {
+    this.props.fetchFeedback();
+  }
+
+  renderFeedbackItems() {
+    const { feedback } = this.props;
+    return feedback.map(fb => {
+      return (
+        <Item key={fb._id}>
+          <Item.Image size="small" src={fb.feedbackUser.imageUrl} />
+          <Item.Content>
+            <Item.Header>{fb.feedbackType.feedbackType}</Item.Header>
+            {fb.feedbackAppVersion && (
+              <Item.Meta>
+                Version {fb.feedbackAppVersion.versionNumber}
+              </Item.Meta>
+            )}
+
+            <Item.Description>{fb.feedbackNotes}</Item.Description>
+            <Item.Extra>
+              Submitted &nbsp;<Moment fromNow>{fb.feedbackDate}</Moment>&nbsp;by{' '}
+              {fb.feedbackUser.firstName} {fb.feedbackUser.lastName}
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      );
+    });
+  }
+
   render() {
-    return <div>List feedback</div>;
+    return (
+      <div>
+        <Header as="h3" textAlign="center">
+          Feedback
+        </Header>
+        <Item.Group>{this.renderFeedbackItems()}</Item.Group>
+      </div>
+    );
   }
 }
 
@@ -13,5 +54,14 @@ const mapStateToProps = state => {
     feedback: selectFeedBackDateDesc(state)
   };
 };
+
+const mapDispatchToProps = {
+  fetchFeedback
+};
+
+AppFeedbackList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppFeedbackList);
 
 export default AppFeedbackList;
