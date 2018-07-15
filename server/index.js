ce6e6a22601252bbcb29797987166a59f7b9723d
Adrian Durran <adrian@intellidroid.eu>
2018-07-15 13:29:23 +0100

    chore: ready for deploy

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     name: 'tmdb',
-    maxAge: 8 * 60 * 60 * 1000, //temp while in dev
+    maxAge: 1 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
     // httpOnly: true,
     // secure: true
