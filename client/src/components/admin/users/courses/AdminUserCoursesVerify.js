e43172acd288f8af8bd7c283bd69a4e8fe9e8368
Adrian Durran <adrian@intellidroid.eu>
2018-07-22 11:39:11 +0100

    feat: added avatar to verify courses

--- client/src/components/admin/users/courses/AdminUserCoursesVerify.js
+++ client/src/components/admin/users/courses/AdminUserCoursesVerify.js
@@ -53,7 +53,11 @@ class AdminUserCoursesVerify extends Component {
     const { data } = this.state;
     return data.map(({ _id, _course, passDate }) => {
       let validText = '';
-      if (_course.validity === undefined) {
+      if (
+        _course.validity === undefined ||
+        _course.validity === '' ||
+        _course.validity === null
+      ) {
         validText = 'No expiry date';
       }
       if (_course.validity > 0) {
