a3738b5d5ddb3899c104c12374eb7b4f88feb4c1
Adrian Durran <adrian@intellidroid.eu>
2018-08-15 10:20:50 +0100

    feat: added numbered labels to admin menu

--- client/src/components/admin/users/AdminUserMenu.js
+++ client/src/components/admin/users/AdminUserMenu.js
@@ -1,32 +1,54 @@
 import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon, Label } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {
+  selectAllUsersVerify,
+  selectAllUsersCoursesVerify,
+  selectAllUsersActive
+} from '../../../reducers/selectors/adminSelectors';
+import { selectDepts } from '../../../reducers/selectors/deptSelectors';
 
-const AdminUserMenu = props => {
+const AdminUserMenu = ({ UsersWaiting, UsersActive, CoursesVerify, Depts }) => {
   return (
-    <Menu attached="top">
+    <Menu compact attached="top">
       <Menu.Item name="Dashboard" as={Link} to="/admin/dashboard">
         <Icon name="home" />
         Admin Dashboard
       </Menu.Item>
-      <Menu.Item
-        name="User Access Manager"
-        as={Link}
-        to="/admin/user-access-manager"
-      />
-      <Menu.Item name="User Manager" as={Link} to="/admin/user-manager" />
-      <Menu.Item
-        name="User Course Manager"
-        as={Link}
-        to="/admin/user-courses-manager"
-      />
-      <Menu.Item
-        name="Department Manager"
-        as={Link}
-        to="/admin/department-manager"
-      />
+      <Menu.Item as={Link} to="/admin/user-access-manager">
+        <Label color="red" floating>
+          {UsersWaiting.length}
+        </Label>
+        User Access Manager
+      </Menu.Item>
+
+      <Menu.Item as={Link} to="/admin/user-manager">
+        <Label floating>{UsersActive.length}</Label>
+        User Manager
+      </Menu.Item>
+
+      <Menu.Item as={Link} to="/admin/user-courses-manager">
+        <Label floating color="red">
+          {CoursesVerify.length}
+        </Label>
+        User Course Manager
+      </Menu.Item>
+      <Menu.Item as={Link} to="/admin/department-manager">
+        <Label floating>{Depts.length}</Label>
+        Department Manager
+      </Menu.Item>
     </Menu>
   );
 };
 
-export default AdminUserMenu;
+const mapStateToProps = (state) => {
+  return {
+    UsersWaiting: selectAllUsersVerify(state),
+    UsersActive: selectAllUsersActive(state),
+    CoursesVerify: selectAllUsersCoursesVerify(state),
+    Depts: selectDepts(state)
+  };
+};
+
+export default connect(mapStateToProps)(AdminUserMenu);
