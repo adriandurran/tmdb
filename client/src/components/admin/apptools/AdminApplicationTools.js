20105cfec9948b798ad9d8f45b3803c450d3eb53
Adrian Durran <adrian@intellidroid.eu>
2018-08-17 11:31:57 +0100

    feat: loaded up on front end dashboard

--- client/src/components/admin/apptools/AdminApplicationTools.js
+++ client/src/components/admin/apptools/AdminApplicationTools.js
@@ -6,7 +6,7 @@ import { Header, Card } from 'semantic-ui-react';
 const AdminApplicationTools = () => {
   return (
     <div>
-      <Card.Group itemsPerRow={2}>
+      <Card.Group itemsPerRow={3}>
         <Card raised as={Link} to="/application/feedback-manager">
           <Card.Content>
             <Header as="h5">Feedback Manager</Header>
@@ -18,6 +18,12 @@ const AdminApplicationTools = () => {
             <Header as="h5">Version Manager</Header>
           </Card.Content>
           <Card.Content description="Manage application versions" />
+        </Card>
+        <Card raised as={Link} to="/admin/dashboard-builder" disabled>
+          <Card.Content>
+            <Header as="h5">Dashboard Builder</Header>
+          </Card.Content>
+          <Card.Content description="Manage application dashboard" />
         </Card>
       </Card.Group>
     </div>
