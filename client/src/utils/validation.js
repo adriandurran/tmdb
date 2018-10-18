836f2b1aaa0eb2799c31833e8a57a66085997311
Adrian Durran <adrian@intellidroid.eu>
2018-10-18 09:02:07 +0800

    chore update to cra v2

--- client/src/utils/validation.js
+++ client/src/utils/validation.js
@@ -1,10 +1,14 @@
-export const required = value => (value ? undefined : 'Required');
+export const required = (value) => (value ? undefined : 'Required');
 
-export const number = value =>
+export const number = (value) =>
   value && isNaN(Number(value)) ? 'Must be a number' : undefined;
 
-export const email = value => {
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-    ? 'Invalid email address'
-    : undefined;
+export const email = (value) => {
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    return true;
+  }
+  return false;
+  // value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+  //   ? 'Invalid email address'
+  //   : undefined;
 };
