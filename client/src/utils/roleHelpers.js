4540dd65e5163f56f7a86fb9ea356f432cfb6d30
Adrian Durran <adrian@intellidroid.eu>
2018-11-03 15:53:40 +0000

    Update client/src/utils/roleHelpers.js
    fixed coding error

--- client/src/utils/roleHelpers.js
+++ client/src/utils/roleHelpers.js
@@ -2,10 +2,8 @@ import { includes } from 'lodash';
 
 // return a list of role users for a given role
 export const roleUsers = (users, roleId) => {
-  return users.filter((user) => {
-    if (user.roles) {
-      return includes(user.roles.map((role) => role._id), roleId);
-    }
+  return users.filter((user) => user.role).filter((user) => {
+    return includes(user.roles.map((role) => role._id), roleId);
   });
 };
 
