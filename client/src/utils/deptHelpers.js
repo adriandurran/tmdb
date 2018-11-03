eb2dd66cf1280db9ad988b7fdea47846433ca6c5
Adrian Durran <adrian@intellidroid.eu>
2018-11-03 15:50:55 +0000

    Update client/src/utils/deptHelpers.js
    fixed coding error

--- client/src/utils/deptHelpers.js
+++ client/src/utils/deptHelpers.js
@@ -1,8 +1,6 @@
 // return list of users for a given dept
 export const deptUsers = (users, deptId) => {
-  return users.filter((user) => {
-    if (user.department) {
-      return user.department._id === deptId;
-    }
-  });
+  return users
+    .filter((user) => user.department)
+    .filter((user) => user.department._id === deptId);
 };
