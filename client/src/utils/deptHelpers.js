1521f2188e4ed3059143a46ffac675f51394dcbd
Adrian Durran <adrian@intellidroid.eu>
2018-11-18 15:16:48 +0000

    feature: added more info to the dept view

--- client/src/utils/deptHelpers.js
+++ client/src/utils/deptHelpers.js
@@ -1,6 +1,18 @@
+import { uniq, map } from 'lodash';
+
 // return list of users for a given dept
 export const deptUsers = (users, deptId) => {
   return users
     .filter((user) => user.department)
     .filter((user) => user.department._id === deptId);
+};
+
+// return a distinct set of roles for a dept of users
+
+export const deptRoles = (users) => {
+  return users
+    .filter((user) => user.roles.length > 0 && user.roles !== null)
+    .map((user) => {
+      return uniq(map(user.roles, '_id'));
+    });
 };
