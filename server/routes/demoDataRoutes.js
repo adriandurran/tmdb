e0f7a785a99f293c94c4ba603ba98c1b85549c6e
Adrian Durran <adrian@intellidroid.eu>
2018-12-01 16:52:25 +0000

    chore: adding in all demo data

--- server/routes/demoDataRoutes.js
+++ server/routes/demoDataRoutes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const demoData = require('../controllers/demoDataController');
 
 router.get('/users', demoData.demoUsers);
+router.get('/depts', demoData.demoDepts);
+router.get('/roles', demoData.demoRoles);
+router.get('/comptypes', demoData.demoCompType);
+router.get('/comps', demoData.demoComps);
 
 module.exports = router;
