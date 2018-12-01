af24ff9345b5004c810e5ed494121f0c4c499781
Adrian Durran <adrian@intellidroid.eu>
2018-12-01 10:45:13 +0000

    Refactor: remove uneeded keys

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,11 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGODB_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  cloudinary: {
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  },
   seedAdmin: {
     userId: process.env.SEEDADMIN_ID,
     firstName: process.env.SEEDADMIN_FN,
