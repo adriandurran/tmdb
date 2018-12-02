c51320fbf2eee2dfeb64ba9212ad879e12b31bb4
Adrian Durran <adrian@intellidroid.eu>
2018-12-02 14:00:47 +0000

    Update demoDataRoutes.js

--- server/routes/demoDataRoutes.js
+++ server/routes/demoDataRoutes.js
@@ -5,11 +5,11 @@ const demoData = require('../controllers/demoDataController');
 
 router.get('/users', demoData.demoUsers);
 router.get('/depts', demoData.demoDepts);
-router.get('/roles', demoData.demoRoles);
-router.get('/comptypes', demoData.demoCompType);
-router.get('/comps', demoData.demoComps);
-router.get('/courses', demoData.demoCourses);
 router.get('/coursetypes', demoData.demoCourseTypes);
 router.get('/courselevels', demoData.demoCourseLevels);
+router.get('/courses', demoData.demoCourses);
+router.get('/comptypes', demoData.demoCompType);
+router.get('/comps', demoData.demoComps);
+router.get('/roles', demoData.demoRoles);
 
 module.exports = router;
