eff6921e35ca2406b8611064a6366fe3cd4fcd70
Adrian Durran <adrian@intellidroid.eu>
2019-10-05 15:13:18 +0100

    client - revised user details card for user manager

---
+++ client/src/components/admin/users/AdminUserDetails.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { isEmpty } from 'lodash';
+import Moment from 'react-moment';
+import { Card, Image } from 'semantic-ui-react';
+
+import { selectUserManage } from '../../../reducers/selectors/adminSelectors';
+
+const AdminUserDetails = () => {
+  const user = useSelector(selectUserManage);
+
+  return (
+    <>
+      {!isEmpty(user) && (
+        <Card centered>
+          <Card.Content>
+            <Image src={user.imageUrl} floated="right" size="tiny" rounded />
+            <Card.Header>
+              {user.firstName} {user.lastName}
+            </Card.Header>
+            <Card.Meta>{user.userId}</Card.Meta>
+            <Card.Description>{user.username}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            Joined <Moment fromNow>{user.joinDate}</Moment>
+          </Card.Content>
+        </Card>
+      )}
+    </>
+  );
+};
+
+export default AdminUserDetails;
--- client/src/components/admin/users/roles/AdminUserRCTools.js
+++ client/src/components/admin/users/roles/AdminUserRCTools.js
@@ -5,12 +5,11 @@ import { Grid } from 'semantic-ui-react';
 
 import { selectUserManage } from '../../../../reducers/selectors/adminSelectors';
 
-import AdminUserDetailsCard from '../AdminUserDetailsCard';
+import AdminUserDetails from '../AdminUserDetails';
 import AdminUserRoleManager from './AdminUserRoleManager';
 import AdminUserComps from '../comps/AdminUserComps';
 import AdminUserCourseTable from '../courses/AdminUserCoursesTable';
 import AdminUserAddDept from '../dept/AdminUserAddDept';
-import AdminUserResetPassword from '../access/AdminUserResetPassword';
 import AdminUserAnalysis from '../analysis/AdminUserAnalysis';
 
 const AdminUserRCTools = () => {
@@ -20,17 +19,13 @@ const AdminUserRCTools = () => {
     <>
       {!isEmpty(user) && (
         <Grid celled>
-          <Grid.Row columns={3}>
-            <Grid.Column>
-              <AdminUserDetailsCard />
-            </Grid.Column>
-
+          <Grid.Row columns={2} centered divided>
             <Grid.Column>
               <AdminUserAddDept />
             </Grid.Column>
 
             <Grid.Column>
-              <AdminUserResetPassword />
+              <AdminUserDetails />
             </Grid.Column>
           </Grid.Row>
           {/* roles */}
